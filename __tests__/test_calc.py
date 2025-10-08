e35d2d2407567fe65fc0cc52b6cee5a314885603
Jake Kohl <jacob.jp.kohl@gmail.com>
2025-10-08 10:54:20 -0500

    feature: basic e2e tests using pytest

---
+++ __tests__/test_calc.py
@@ -0,0 +1,12 @@
+class TestCalculator:
+  def test_addition(self):
+    assert 1 + 1 == 2
+
+  def test_subtraction(self):
+    assert 1 - 1 == 0
+
+  def test_multiplication(self):
+    assert 1 * 1 == 1
+
+  def test_division(self):
+    assert 1 / 1 == 1
